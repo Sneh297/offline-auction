27256ed0f309d90ee43abc280b7fe73ce2be07c3
Sneh297 <snehdholia@gmail.com>
2026-03-07 23:06:32 +0530

    fix netlify routing

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -1,38 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import URL from "../utils/URL";
 
 const ProtectedRoute = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
 
-useEffect(() => {
-  const checkLicense = async () => {
-    try {
-      const res = await fetch(`${URL}/check-license`, {
-        credentials: "include",
-      });
+  useEffect(() => {
+    const checkLicense = async () => {
+      try {
+        const res = await fetch(`${URL}/check-license`, {
+          credentials: "include",
+        });
 
-      const data = await res.json();
+        const data = await res.json();
 
-      if (data.valid) {
-        navigate("/dashboard", { replace: true });
-      } else {
+        setIsAuthenticated(data.valid);
+      } catch (err) {
+        console.error(err);
         setIsAuthenticated(false);
       }
-    } catch (err) {
-      console.error(err);
-      setIsAuthenticated(false);
-    }
-  };
+    };
 
-  checkLicense();
-}, [navigate]);
+    checkLicense();
+  }, []);
 
-  // Show loading while checking cookie
   if (isAuthenticated === null) return <div>Loading...</div>;
 
   if (!isAuthenticated) return <Navigate to="/" replace />;
 
-  // Use <Outlet /> to render nested protected routes
   return <Outlet />;
 };
 
