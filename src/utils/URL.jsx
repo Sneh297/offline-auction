b79c352aeb30c551124d7b6a3fee86371f69db14
Sneh297 <snehdholia@gmail.com>
2026-03-08 16:37:40 +0530

    done-pdf

--- src/utils/URL.jsx
+++ src/utils/URL.jsx
@@ -1,3 +1,3 @@
-// const  URL = "https://offline-auction-backend.onrender.com"
-const URL = "http://localhost:5000"
+const  URL = "https://offline-auction-backend.onrender.com"
+// const URL = "http://localhost:5000"
 export default URL;
